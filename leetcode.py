7786f7daeedd3d484bdada9d020a66464244a1fc
SRINIVASAN <srini172003@gmail.com>
2025-06-07 08:01:46 +0530

    Leetcode python file
    Leetcode python file solved problem

---
+++ leetcode.py
@@ -0,0 +1,22 @@
+class Solution:
+    def clearStars(self, s: str) -> str:
+        ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+        d = {letter: [] for letter in ALPHABET}
+
+        def getSmallestLetter():
+            for letter in ALPHABET:
+                if d[letter]:
+                    return letter
+            # Should always exist a solution!
+            assert False
+
+        deleted_indices = set()
+        for i, char in enumerate(s):
+            if char == "*":
+                smallest_letter = getSmallestLetter()
+                deleted_indices.add(d[smallest_letter].pop())
+                continue
+            
+            d[char].append(i)
+        
+        return "".join(char for i, char in enumerate(s) if char != "*" and i not in deleted_indices)
