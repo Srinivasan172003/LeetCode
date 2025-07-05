905ac286fa1f602314ea261341286115f59ff5a3
SRINIVASAN <srini172003@gmail.com>
2025-07-05 22:41:46 +0530

    Leet code 05-07-2025
    Find Lucky Integer in an Array

--- leetcode (2).java
+++ leetcode (2).java
@@ -1,36 +1,17 @@
 class Solution {
-    public char kthCharacter(long k, int[] operations) {
-        // Base case: if k is 1, return 'a'
-        if (k == 1) {
-            return 'a';
-        }
-
-        int n = operations.length; // Length of the operations array
-        int opType = 0; // To store the operation type (0 or 1)
-        long len = 1; // Start with length 1 (initial 'a')
-        long newK = k; // To store the adjusted k value
-
-        // Determine the operation type by traversing the operations array
+    public int findLucky(int[] arr) {
+        int n = arr.length;
         for (int i = 0; i < n; i++) {
-            len *= 2; // Double the length
-            if (len >= k) { // Check if the length has surpassed k
-                opType = operations[i]; // Get the current operation type
-                newK = k - len / 2; // Adjust k for the recursive call
-                break;
+            int val = arr[i] & 65535; // Lower 16 bits
+            if (val >= 1 && val <= n) {
+                arr[val - 1] += (1 << 16); // Increase high 16 bits
             }
         }
-
-        // Recursive call to find the character before the operation is applied
-        char res = kthCharacter(newK, operations);
-
-        // If operation type is 0, return the result as is
-        if (opType == 0) 
-            return res;
-
-        // If operation type is 1, transform the character
-        if (res == 'z') 
-            return 'a';
-
-        return (char) (res + 1); // Increment character by 1
+        for (int val = n; val >= 1; val--) {
+            if ((arr[val - 1] >> 16) == val) {
+                return val;
+            }
+        }
+        return -1;
     }
 }
